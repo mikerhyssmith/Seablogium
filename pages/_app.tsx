401d885fdc3d02eca38edafe0639a88d43704761
Mike Smith <mikerhyssmith@googlemail.com>
2021-01-30 10:09:36 +0000

    add react plugin compose

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,33 @@
 import "../styles/globals.css";
-import { ChakraProvider } from "@chakra-ui/react";
+import { ChakraProvider, Grid, Box, extendTheme } from "@chakra-ui/react";
+import { Global } from "@emotion/react";
+import fonts from "../lib/fonts";
+import Icon from "./Seablogium.svg";
+
+const theme = extendTheme({
+  colors: {
+    brand: {
+      100: "#f7fafc",
+      // ...
+      900: "#136F63",
+    },
+  },
+  fonts: {
+    body: "'Nunito', sans-serif",
+    heading: "Georgia, serif",
+  },
+});
 
 function MyApp({ Component, pageProps }) {
   return (
-    <ChakraProvider>
-      <Component {...pageProps} />
+    <ChakraProvider theme={theme}>
+      <Global styles={fonts} />
+      <Grid h="100vh" templateColumns="20% 1fr" gap={4}>
+        <Box bg="brand.900" w="100%" p={4} color="white">
+          This is the Box
+        </Box>
+        <Component {...pageProps} />
+      </Grid>
     </ChakraProvider>
   );
 }
