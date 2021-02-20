592c34e6281688805bfb92ab940cd301bd617c8d
Mike Smith <mikerhyssmith@googlemail.com>
2021-02-20 19:11:19 +0000

    Add analysis scripts folder

---
+++ analysis-scripts/analysis_scripts/savings.py
@@ -0,0 +1,15 @@
+import pandas as pd
+
+savings_df = pd.read_csv('./data/FIRE-Savings.csv')
+income_df = pd.read_csv('./data/FIRE-Income.csv')
+
+savings_df["Date"] = pd.to_datetime(savings_df["Date"], dayfirst=True)
+income_df["Date"] = pd.to_datetime(income_df["Date"], dayfirst=True)
+
+sum_by_month = savings_df.groupby(pd.Grouper(key='Date', freq='MS')).sum()
+
+concatenated = pd.merge(left=sum_by_month, right=income_df, how="left", on="Date")
+
+concatenated["percentage"] = concatenated["Saved"]/concatenated["Income"] * 100
+
+print(concatenated)
